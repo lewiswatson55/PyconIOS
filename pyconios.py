4c546b136f16816db810524e8ffa8017c53f9597
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-02 17:58:40 +0100

    not working

--- pyconios.py
+++ pyconios.py
@@ -1,2 +1,21 @@
-print("Hello World")
+#Python program for generating ios icons
 
+import sys
+import os
+from PIL import Image
+
+
+#im = Image.open(inputimage)
+
+
+def checkinput():
+    if (len(sys.argv) >= 2):
+        print('Attempting to open input image:', str(sys.argv[1]))
+        inputimage = sys.argv[1]
+        im = Image.open(inputimage)
+
+    else:
+        print("Please specify an input image i.e try:\n pyconios.py testicon.png")
+
+
+#checkinput()
