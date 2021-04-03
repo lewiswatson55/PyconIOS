8b6b31aee825ae925a6a1261e41f02e7b447350a
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-03 17:41:41 +0100

    Added input verification and generation of icons

--- pyconios.py
+++ pyconios.py
@@ -4,18 +4,43 @@ import sys
 import os
 from PIL import Image
 
+im = None
 
-#im = Image.open(inputimage)
-
-
-def checkinput():
-    if (len(sys.argv) >= 2):
-        print('Attempting to open input image:', str(sys.argv[1]))
+def getinput():
+    global im
+    try:
         inputimage = sys.argv[1]
         im = Image.open(inputimage)
+    except:
+        print("\nFailed to open photo...\nPlease specify an input image i.e try:\n    pyconios.py testicon.png")
+        exit()
 
-    else:
-        print("Please specify an input image i.e try:\n pyconios.py testicon.png")
+def resizeimage(size):
+    #resize image
+    out = im.resize((size,size))
+    out.save('./output/icon_{0}.png'.format(size))
 
 
-#checkinput()
+def generateicons(requiredsizes):
+
+    print("Generating images...")
+    for i in range(len(requiredsizes)):
+        resizeimage(requiredsizes[i])
+    print("Finished.")
+
+
+def main():
+
+    #Required sizes list
+    requiredsizes = [40, 60, 58, 87, 80, 120, 180, 20, 29, 58, 76, 152, 167, 1024]
+
+    #Verify Image Imput
+    getinput()
+
+    #Generate icons from list
+    generateicons(requiredsizes)
+
+
+
+if __name__ == "__main__":
+    main()
